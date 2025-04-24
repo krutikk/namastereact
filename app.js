bd35d18ddc7b71ab69b45213e19aeffc34702bc1
krutikk <krutik.khandhadiya@gmail.com>
2025-04-24 22:20:38 +0800

    episode-1

---
+++ app.js
@@ -0,0 +1,15 @@
+const html = React.createElement;
+const root = ReactDOM.createRoot(document.getElementById("root"));
+const parent = html("div", { id: "parent", key: "parent" }, [
+        html("div", { id: "child1", key: "child1" }, [
+                html("h1", { id: "heading", key: "h1child1" }, "I am H1 tag"),
+                html("h2", { id: "sub-heading", key: "h2child1" }, "I am H2 tag"),
+        ]),
+        html("div", { id: "child2", key: "child2" }, [
+                html("h1", { id: "heading", key: "h1child2" }, "Namaste React from react script"),
+                html("h2", { id: "sub-heading", key: "h2child2" }, "Namaste React from react script"),
+        ]),
+]);
+const heading = html("h1", { id: "heading" }, "Namaste React from react script");
+console.log(heading);
+root.render(parent);
