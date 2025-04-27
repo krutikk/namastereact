c7e9f6b4b642c33eebad9c3333c76a0f05dcf1df
krutikk <krutik.khandhadiya@gmail.com>
2025-04-27 08:32:51 +0800

    Episode3

--- app.js
+++ app.js
@@ -2,16 +2,30 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 const html = React.createElement;
 const root = ReactDOM.createRoot(document.getElementById("root"));
-const parent = html("div", { id: "parent", key: "parent" }, [
-        html("div", { id: "child1", key: "child1" }, [
-                html("h1", { id: "heading", key: "h1child1" }, "I am H1 tag"),
-                html("h2", { id: "sub-heading", key: "h2child1" }, "I am H2 tag"),
-        ]),
-        html("div", { id: "child2", key: "child2" }, [
-                html("h1", { id: "heading", key: "h1child2" }, "Namaste React from react script"),
-                html("h2", { id: "sub-heading", key: "h2child2" }, "Namaste React from react script"),
-        ]),
-]);
-const heading = html("h1", { id: "heading" }, "Namaste React from react script");
-console.log(heading);
-root.render(parent);
+
+const heading = html(
+  "h1",
+  { id: "heading" },
+  "Namaste React from react script"
+);
+
+const jsxheading = (
+  <h1 id="heading">Namaste React from react script using jsx</h1>
+); //html like syntax
+
+//js engine ES6 Ecmascript
+
+//React.createElement => React element => html element
+//ReactDOM.render => React element => html element => browser
+const Title = () => (<h2> React using JS</h2>);
+
+const data = 1000;
+const HeadingComponent = () => (
+  <div id="container">
+    <Title> </Title>
+    data: {data}
+    <h1 className="heading"> Namaste React from react functional using jsx</h1>
+  </div>
+);
+
+root.render(<HeadingComponent />); //it became html element
